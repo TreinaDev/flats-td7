702f0a9c19fbc9d228febe4500c11df23abd538c
Joao Almeida <joaorsalmeida@gmail.com>
2021-09-16 17:22:01 -0300

    Add exercises

--- spec/system/visitor_view_properties_spec.rb
+++ spec/system/visitor_view_properties_spec.rb
@@ -26,4 +26,30 @@ describe 'Visitor visit homepage' do
     expect(page).to have_text("Quartos: 5")
   end
 
+  it 'and theres no property available' do
+    #Act => Agir (executar a funcionalidade)
+    visit root_path
+    expect(page).to have_text("Nenhum imóvel disponível")
+  end
+
+  it 'and view property details' do
+    #Arrange => Preparar (os dados)
+    Property.create({ title: 'Casa com quintal em Copacabana', 
+                      description: 'Excelente casa, recém reformada com 2 vagas de garagem',
+                      rooms: 3, parking_slot: true, bathrooms: 2, pets: true, daily_rate: 500
+                    })
+
+    visit root_path
+    click_on 'Casa com quintal em Copacabana'
+
+    expect(page).to have_text("Casa com quintal em Copacabana")
+    expect(page).to have_text("Excelente casa, recém reformada com 2 vagas de garagem")
+    expect(page).to have_text("Quartos: 3")
+    expect(page).to have_text("Banheiros: 2")
+    expect(page).to have_text("Aceita Pets: Sim")
+    expect(page).to have_text("Estacionamento: Sim")
+    expect(page).to have_text("Diária: R$ 500")
+
+  end
+
 end
