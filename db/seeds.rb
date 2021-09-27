756a1d99511322a8ddae8ff45f49055274849473
Joao Almeida <joaorsalmeida@gmail.com>
2021-09-27 16:35:15 -0300

    Adiciona arquivo de seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+casa = PropertyType.create!(name: 'Casa')
+Property.create!({ title: 'Casa com quintal em Copacabana', 
+                  description: 'Excelente casa, recém reformada com 2 vagas de garagem',
+                  rooms: 3, parking_slot: true, bathrooms: 2, daily_rate: 150,
+                  property_type: casa
+                })
+
+apartamento = PropertyType.create!(name: 'Apartamento')
+Property.create!({ title: 'Cobertura em Manaus', 
+                  description: 'Cobertura de 300m2, churrasqueira e sauna privativa',
+                  rooms: 5, parking_slot: false, bathrooms: 2, daily_rate: 150,
+                  property_type: apartamento
+                })
