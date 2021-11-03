442e70389e3410207dfc12a73352a5f41d331446
Joao Almeida <joaorsalmeida@gmail.com>
2021-11-03 16:44:01 -0300

    Configurando SimpleCov

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara'
   gem 'rspec-rails'
+  gem 'simplecov', require: false
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,11 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/helpers/'
+  add_filter '/jobs/'
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
